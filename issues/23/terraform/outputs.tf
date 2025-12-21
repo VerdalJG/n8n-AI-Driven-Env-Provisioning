c5650170b2f1c553830d6bfae131d555239cbd7e
VerdalJG <88113293+VerdalJG@users.noreply.github.com>
2025-12-21 17:28:20 +0100

    Delete issues/23 directory

--- issues/23/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dev_instance_public_ip" {
-  description = "Public IP of the dev EC2 instance"
-  value       = aws_instance.dev.public_ip
-}
-
-output "rds_endpoint" {
-  description = "Endpoint of the RDS database"
-  value       = aws_db_instance.postgresql.endpoint
-}
