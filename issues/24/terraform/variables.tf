b0037a2a349a268411500a8210b650a768d2af5d
VerdalJG <88113293+VerdalJG@users.noreply.github.com>
2025-12-21 20:51:07 +0100

    chore(n8n) provision - issue 24

---
+++ issues/24/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "dev_env_public_key" {
+    type = string
+}
+
+variable "issue_number" {
+    type = string
+}
+
+variable "db_username" {
+    type = string
+}
+
+variable "db_password" {
+    type = string
+}
+
+variable "github_runner_key" {
+    type = string
+}
